3b4f595ab6f25d82a41bc9af4b944c094d51de5f
Dawn <wings-of-dawn@live.com>
2011-11-19 19:15:56 -0700

    First attempt at staggered tab-loading. Completely untested (no internet access at time of writing.)

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
+var submissionsToOpen;
+var loadingTabs;
+
 function openSubmission(submission)
 {
-	chrome.tabs.create({
+	// Create a new tab to display the submission page
+	var newTab = chrome.tabs.create({
 		url:		submission,
 		selected:	false
 	});
+
+	// Add the tab's id to the list of tabs that are currently loading
+	loadingTabs.push(newTab.id);
 }
 
 chrome.pageAction.onClicked.addListener(function(tab) {
@@ -11,18 +18,36 @@ chrome.pageAction.onClicked.addListener(function(tab) {
 });
 
 chrome.extension.onConnect.addListener(function(port) {
+	// Set up a listener to receive a list of submission-page links from the content script
 	port.onMessage.addListener(function(submissions) {
-		submissions.forEach(openSubmission);
+		// Hold a reference to the submissions
+		submissionsToOpen = submissions;
+
+		// Start opening tabs for the submissions
+		for (var i = 0; (i < localStorage["numconcurrent"]) && (submissionsToOpen.length > 0); i++)
+			openSubmission(submissionsToOpen.shift());
 	});
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-	if (change.status == "complete")
+	// We're only interested in pages that have completely loaded
+	if (change.status !== "complete")
+		return;
+
+	// If we've just loaded a page with submission thumbnails, show the "open all" icon
+	if (tab.url.indexOf(".furaffinity.net/msg/submissions") != -1)
 	{
-		if (tab.url.indexOf(".furaffinity.net/msg/submissions") != -1)
-		{
-			chrome.pageAction.show(tabId);
-		}
+		chrome.pageAction.show(tabId);
+	}
+	// If the newly-loaded page is a tab opened by this extension, check if we should load the next submission
+	else if ((var index = loadingTabs.indexOf(tabId)) > -1)
+	{
+		// Remove the completed tab from our set of loading tabs
+		loadingTabs.splice(index, 1);
+
+		// If we have more submissions to open, start the next one
+		if (submissionsToOpen.length > 0)
+			openSubmission(submissionsToOpen.shift());
 	}
 });
 
