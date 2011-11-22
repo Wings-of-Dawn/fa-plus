757c280ae80d34a4de550b40428f993e61afb16a
Dawn <wings-of-dawn@live.com>
2011-11-22 01:56:45 -0500

    [Cosmetic] Renamed some files to a more logical configuration.

--- options.js
+++ options.js
@@ -1,73 +1,99 @@
-window.addEventListener("load", init, false);
+var OPTION_TYPE = {
+    BOOLEAN:    0,
+    INTEGER:    1,
+    FLOAT:      2,
+    STRING:     3
+};
+var OPTIONS = {
+    OPEN_GENERAL: {
+        key:            "general",
+        type:           OPTION_TYPE.BOOLEAN,
+        defaultValue:   true
+    },
+    OPEN_MATURE: {
+        key:            "mature",
+        type:           OPTION_TYPE.BOOLEAN,
+        defaultValue:   true
+    },
+    OPEN_ADULT: {
+        key:            "adult",
+        type:           OPTION_TYPE.BOOLEAN,
+        defaultValue:   true
+    },
+    TAB_COUNT: {
+        key:            "tab-count",
+        type:           OPTION_TYPE.INTEGER,
+        defaultValue:   5
+    },
+    AUTO_REMOVE: {
+        key:            "auto-remove",
+        type:           OPTION_TYPE.BOOLEAN,
+        defaultValue:   false
+    }
+};
 
-function init()
+// For convenient enumeration:
+var ALL_OPTIONS = [
+    OPTIONS.OPEN_GENERAL,
+    OPTIONS.OPEN_MATURE,
+    OPTIONS.OPEN_ADULT,
+    OPTIONS.TAB_COUNT,
+    OPTIONS.AUTO_REMOVE
+];
+
+function getOptionValue(option)
 {
-    // Restore settings values from localStorage
-    restoreOptions();
+    // Load the option value from localStorage
+    var value = localStorage[option.key];
 
-    // Add an event listener to the save button
-    document.getElementById("savebutton").addEventListener("click", saveOptions, false);
-}
-
-// Populates options page with values loaded from localStorage
-function restoreOption(option)
-{
-    // Find the corresponding element
-    var element = document.getElementById(option.key);
-    if (!element)
+    // If the value doesn't exist, set to default
+    if (!value)
     {
-        console.warn("no corresponding element for option with key \"" + option.key + "\"");
-        return;
+        value = option.defaultValue;
+        localStorage[option.key] = option.defaultValue;
     }
 
-    // Populate the element
-    element[getOptionElementProperty(option)] = getOptionValue(option);
-}
-
-function restoreOptions()
-{
-    // Enumerate options, populating the form
-    ALL_OPTIONS.forEach(restoreOption);
-}
-
-// Saves options to localStorage
-function saveOption(option)
-{
-    // Find the corresponding element
-    var element = document.getElementById(option.key);
-    if (!element)
+    // Convert to the appropriate type, if necessary
+    switch (option.type)
     {
-        console.warn("no corresponding element for option with key \"" + option.key + "\"");
-        return;
+        case OPTION_TYPE.BOOLEAN:
+        {
+            // Check if the value is boolean "false", or a string representation thereof
+            if (!value || (value === "false"))
+                value = false;
+            else
+                value = true;
+            break;
+        }
+        case OPTION_TYPE.INTEGER:
+        {
+            value = parseInt(value);
+            break;
+        }
+        case OPTION_TYPE.FLOAT:
+        {
+            value = parseFloat(value);
+            break;
+        }
+        default:
+            break;
     }
 
-    // Set the option to the value of the element's value
-    setOptionValue(option, element[getOptionElementProperty(option)]);
+    // Return the value
+    return value;
 }
 
-function saveOptions()
+function setOptionValue(option, value)
 {
-    // Check that the tab-count field contains an integer between 1 and 60
-    var tabCountVal = parseInt(document.getElementById(OPTIONS.TAB_COUNT.key).value);
-    var errorElement = document.getElementById("tab-count-error");
-    if (isNaN(tabCountVal) || (tabCountVal < 1) || (tabCountVal > 60))
-    {
-        // Display an error message
-        errorElement.innerHTML = "Please enter a value in the range 1-60";
-        return;
-    }
-
-    // Enumerate options, saving each to local storage
-    ALL_OPTIONS.forEach(saveOption);
-
-    // Remove error message, if any
-    errorElement.innerHTML = "";
-
-    // Update status to let user know options were saved.
-    var status = document.getElementById("status");
-    status.innerHTML = "<p>Options saved.</p>";
-    setTimeout(function() {
-            status.innerHTML = "";
-    }, 2000);
+    localStorage[option.key] = value;
 }
 
+function getOptionElementProperty(option)
+{
+    switch (option.type)
+    {
+        case OPTION_TYPE.BOOLEAN:
+            return "checked";
+    }
+    return "value";
+}
---
+++ options_page.js
@@ -0,0 +1,73 @@
+window.addEventListener("load", init, false);
+
+function init()
+{
+    // Restore settings values from localStorage
+    restoreOptions();
+
+    // Add an event listener to the save button
+    document.getElementById("savebutton").addEventListener("click", saveOptions, false);
+}
+
+// Populates options page with values loaded from localStorage
+function restoreOption(option)
+{
+    // Find the corresponding element
+    var element = document.getElementById(option.key);
+    if (!element)
+    {
+        console.warn("no corresponding element for option with key \"" + option.key + "\"");
+        return;
+    }
+
+    // Populate the element
+    element[getOptionElementProperty(option)] = getOptionValue(option);
+}
+
+function restoreOptions()
+{
+    // Enumerate options, populating the form
+    ALL_OPTIONS.forEach(restoreOption);
+}
+
+// Saves options to localStorage
+function saveOption(option)
+{
+    // Find the corresponding element
+    var element = document.getElementById(option.key);
+    if (!element)
+    {
+        console.warn("no corresponding element for option with key \"" + option.key + "\"");
+        return;
+    }
+
+    // Set the option to the value of the element's value
+    setOptionValue(option, element[getOptionElementProperty(option)]);
+}
+
+function saveOptions()
+{
+    // Check that the tab-count field contains an integer between 1 and 60
+    var tabCountVal = parseInt(document.getElementById(OPTIONS.TAB_COUNT.key).value);
+    var errorElement = document.getElementById("tab-count-error");
+    if (isNaN(tabCountVal) || (tabCountVal < 1) || (tabCountVal > 60))
+    {
+        // Display an error message
+        errorElement.innerHTML = "Please enter a value in the range 1-60";
+        return;
+    }
+
+    // Enumerate options, saving each to local storage
+    ALL_OPTIONS.forEach(saveOption);
+
+    // Remove error message, if any
+    errorElement.innerHTML = "";
+
+    // Update status to let user know options were saved.
+    var status = document.getElementById("status");
+    status.innerHTML = "<p>Options saved.</p>";
+    setTimeout(function() {
+            status.innerHTML = "";
+    }, 2000);
+}
+
