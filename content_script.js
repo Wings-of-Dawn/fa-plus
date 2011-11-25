6ec4b9d6a3d7b8fd06dbd88cc4981b3de8fec832
Dawn <wings-of-dawn@live.com>
2011-11-24 23:12:38 -0800

    Fixed submissions of mixed ratings opening in the wrong order.

--- content_script.js
+++ content_script.js
@@ -4,31 +4,38 @@ chrome.extension.onRequest.addListener(function (request, sender, sendResponse) 
 
 function findSubmissions(submissionTypes)
 {
+    var SUBMISSION_THUMB_CLASS_NAME = "thumb-overlay";
 	var foundSubmissions = [];
-    submissionTypes.forEach(function (type) {
-	    // Find the thumbnails for all submissions of each type we want to open
-        var submissionThumbs = document.getElementsByClassName(type);
 
-        // For each thumbnail, find the reference to the corresponding submission page
-        for (var i = 0; i < submissionThumbs.length; i++)
+    // Find the thumbnail-container elements for all submissions on the page
+    var submissionThumbs = document.getElementsByClassName(SUBMISSION_THUMB_CLASS_NAME);
+
+    // For each thumbnail, check if it passes our filter, and find the reference to the corresponding submission page
+    for (var i = 0; i < submissionThumbs.length; i++)
+    {
+        // Check if the thumbnail's classes include one of the allowed submission types
+        var thumbnail = submissionThumbs[i];
+        var allowed = submissionTypes.some(function (type) {
+            return (thumbnail.className.indexOf(type) > 0);
+        });
+        if (!allowed)
+            continue;
+
+        // Get the container around the thumbnail
+        var submissionContainer = thumbnail.parentNode;
+
+        // Find all anchor (link) elements in the container node (should be exactly 1)
+        var anchors = submissionContainer.getElementsByTagName('a');
+        if (anchors.length !== 1)
         {
-            // Get the container around the thumbnail
-            var submissionContainer = submissionThumbs[i].parentNode;
-
-            // Find all anchor (link) elements in the container node (should be exactly 1)
-            var anchors = submissionContainer.getElementsByTagName('a');
-            if (anchors.length !== 1)
-            {
-                console.warn("Unexpected number of anchor elements in submission container element: " + anchors.length + " (expected 1)");
-                if (anchors.length < 1)
-                    continue;
-            }
-
-            // Add the first anchor found to the list of submission-page links to open
-            foundSubmissions.push(anchors[0].href);
+            console.warn("Unexpected number of anchor elements in submission container element: " + anchors.length + " (expected 1)");
+            if (anchors.length < 1)
+                continue;
         }
-    });
+
+        // Add the first anchor found to the list of submission-page links to open
+        foundSubmissions.push(anchors[0].href);
+    }
 
 	return foundSubmissions;
 }
-
