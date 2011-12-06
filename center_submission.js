3e31cc737f78a29d8dfaa5e8c9b6e312a592c499
Dawn <wings-of-dawn@live.com>
2011-12-06 02:19:30 -0500

    Changed the submission-centering script to align to the center of the image, not the top.

--- center_submission.js
+++ center_submission.js
@@ -1,8 +1,23 @@
 var SUBMISSION_ELEMENT_ID = "submissionImg";
-setTimeout(function () {
+
+function centerViewOnSubmission()
+{
+    debugger;
     var submissionElement = document.getElementById(SUBMISSION_ELEMENT_ID);
-    if (submissionElement)
-        submissionElement.scrollIntoView();
-    else
+    if (!submissionElement)
+    {
         console.warn("cannot scroll to submission: no submission element found");
-}, 500);
+        return;
+    }
+
+    // Determine which coordinates should be placed in the top-left of the window
+    var targetX = submissionElement.x - ((document.documentElement.clientWidth - submissionElement.width) / 2);
+    var targetY = submissionElement.y - ((document.documentElement.clientHeight - submissionElement.height) / 2);
+
+    // Scroll the window to the target point
+    window.scrollTo(targetX, targetY);
+}
+
+// Wait a short time to ensure everything is loaded before attempting to center the submission
+setTimeout(centerViewOnSubmission, 500);
+
