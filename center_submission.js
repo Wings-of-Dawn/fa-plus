8182ee618d316777271c3802215395b65e60c37b
Dawn <wings-of-dawn@live.com>
2017-12-31 18:18:17 -0800

    Fixed submission-centering logic.

--- center_submission.js
+++ center_submission.js
@@ -1,16 +1,16 @@
-var SUBMISSION_ELEMENT_ID = "submissionImg";
+const SUBMISSION_ELEMENT_ID = "submissionImg";
 
 function centerViewOnSubmission() {
-  debugger;
-  var submissionElement = document.getElementById(SUBMISSION_ELEMENT_ID);
+  const submissionElement = document.getElementById(SUBMISSION_ELEMENT_ID);
   if (!submissionElement) {
     console.warn("cannot scroll to submission: no submission element found");
     return;
   }
 
   // Determine which coordinates should be placed in the top-left of the window
-  var targetX = submissionElement.x - ((document.documentElement.clientWidth - submissionElement.width) / 2);
-  var targetY = submissionElement.y - ((document.documentElement.clientHeight - submissionElement.height) / 2);
+  const boundingRect = submissionElement.getBoundingClientRect();
+  const targetX = boundingRect.x + ((boundingRect.width - document.documentElement.clientWidth) / 2);
+  const targetY = boundingRect.y + ((boundingRect.height - document.documentElement.clientHeight) / 2);
 
   // Scroll the window to the target point
   window.scrollTo(targetX, targetY);
