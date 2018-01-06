ca745b6e57e2be7c19f0ea556443768e5184bf9a
Dawn <wings-of-dawn@live.com>
2018-01-05 22:54:36 -0800

    Cleaned up options_page.js

--- options_page.js
+++ options_page.js
@@ -1,35 +1,38 @@
+const MIN_TAB_COUNT = 1;
+const MAX_TAB_COUNT = 72;
+
 window.addEventListener("load", init, false);
 
 function init() {
-  // Restore settings values from localStorage
+  // Restore settings values from storage.
   restoreOptions();
 
-  // Add an event listener to the save button
+  // Add an event listener to the save button.
   document.getElementById("savebutton").addEventListener("click", saveOptions, false);
 }
 
-// Populates options page with values loaded from localStorage
+// Populates options page with values loaded from storage.
 function restoreOption(option) {
-  // Find the corresponding element
-  var element = document.getElementById(option.key);
+  // Find the corresponding element.
+  const element = document.getElementById(option.key);
   if (!element) {
     console.warn("no corresponding element for option with key \"" + option.key + "\"");
     return;
   }
 
-  // Populate the element
+  // Populate the element.
   element[getOptionElementProperty(option)] = getOptionValue(option);
 }
 
 function restoreOptions() {
-  // Enumerate options, populating the form
+  // Enumerate options, populating the form.
   ALL_OPTIONS.forEach(restoreOption);
 }
 
 // Saves options to localStorage
 function saveOption(option) {
-  // Find the corresponding element
-  var element = document.getElementById(option.key);
+  // Find the corresponding element.
+  const element = document.getElementById(option.key);
   if (!element) {
     console.warn("no corresponding element for option with key \"" + option.key + "\"");
     return;
@@ -40,25 +43,24 @@ function saveOption(option) {
 }
 
 function validTabCount(value) {
-  return (!isNaN(value) && (value >= 1) && (value <= 60));
+  return (!isNaN(value) && (value >= MIN_TAB_COUNT) && (value <= MAX_TAB_COUNT));
 }
 
 function saveOptions() {
-  // Remove old error messages, if any
-  var loadCountError = document.getElementById("load-count-error");
-  var tabCountError = document.getElementById("tab-count-error");
+  // Remove old error messages, if any.
+  const loadCountError = document.getElementById("load-count-error");
+  const tabCountError = document.getElementById("tab-count-error");
   loadCountError.innerHTML = "";
   tabCountError.innerHTML = "";
 
-  // Check that the tab-count fields contain integers between 1 and 60
+  // Check that the tab-count fields contain acceptable values.
+  const errorMessage = "Please enter a value in the range " + MIN_TAB_COUNT + "-" + MAX_TAB_COUNT;
   if (!validTabCount(document.getElementById(OPTIONS.LOAD_COUNT.key).value)) {
-    // Display an error message
-    loadCountError.innerHTML = "Please enter a value in the range 1-60";
+    loadCountError.innerHTML = errorMessage;
     return;
   }
   if (!validTabCount(document.getElementById(OPTIONS.TAB_COUNT.key).value)) {
-    // Display an error message
-    tabCountError.innerHTML = "Please enter a value in the range 1-60";
+    tabCountError.innerHTML = errorMessage;
     return;
   }
 
